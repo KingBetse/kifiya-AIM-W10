dcda577d76b1d2f0551e9cc413207b2cc454b54e
betselot <king.betse@gmail.com>
2024-11-01 22:27:15 +0300

    changing test cases

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -5,14 +5,18 @@ class TestBrentOilDataAnalysis(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        # Load the dataset for testing
-        cls.df = pd.read_csv('../data/brentOilPrices.csv')
-    
+        # Create a mock DataFrame for testing
+        data = {
+            'Date': ['20-May-87', '21-May-87', '22-May-87'],
+            'Price': [20.0, 25.0, 30.0]
+        }
+        cls.df = pd.DataFrame(data)
+
     def test_data_loading(self):
-        """ Test if the data is loaded correctly """
+        """ Test if the DataFrame is created correctly """
         self.assertIsInstance(self.df, pd.DataFrame)
         self.assertFalse(self.df.empty)
-        
+
     def test_column_names(self):
         """ Test if the DataFrame has the correct columns """
         expected_columns = ['Date', 'Price']
@@ -29,8 +33,8 @@ class TestBrentOilDataAnalysis(unittest.TestCase):
 
     def test_filter_high_prices(self):
         """ Test filtering prices greater than a certain value """
-        high_prices = self.df[self.df['Price'] > 100]
-        self.assertTrue((high_prices['Price'] > 100).all())
+        high_prices = self.df[self.df['Price'] > 25]
+        self.assertTrue((high_prices['Price'] > 25).all())
 
 if __name__ == '__main__':
     unittest.main()
